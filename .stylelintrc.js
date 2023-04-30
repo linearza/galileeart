5ef006f2ef3f88a01c9204f125928e6e4d0315d3
linearza <vanwijk.mc@gmail.com>
2023-04-30 18:00:15 +0200

    chore: fix linting

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -3,11 +3,14 @@
 module.exports = {
   extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
   rules: {
-    "at-rule-no-unknown": [ true, {
-      ignoreAtRules: [
-        "extends",
-        "tailwind"
-      ]
-    }],
-  }
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['extends', 'tailwind'],
+      },
+    ],
+    'block-no-empty': null,
+    'no-invalid-position-at-import-rule': null,
+    'selector-class-pattern': null,
+  },
 };
