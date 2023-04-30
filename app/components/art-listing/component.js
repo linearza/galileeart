0b53621a8c82bb8db22280af0001e25fd7eba9f5
linearza <vanwijk.mc@gmail.com>
2023-04-30 17:34:28 +0200

    feat: add new art-listing component

--- app/components/art-listing/component.js
+++ app/components/art-listing/component.js
@@ -1,3 +1,7 @@
 import Component from '@glimmer/component';
+import { service } from '@ember/service';
 
-export default class ArtListingComponent extends Component {}
+export default class ArtListingComponent extends Component {
+  // Services
+  @service('airtable') airtableService;
+}
